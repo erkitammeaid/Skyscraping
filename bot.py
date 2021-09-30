8d5fdb4c1b7809fc7321133cfab4455ef286adf2
erki.tammeaid <erkipoiss01@gmail.com>
2021-09-30 15:07:23 +0300

    .

--- bot.py
+++ bot.py
@@ -1,6 +1,21 @@
 import requests
 import sched, time
+import smtplib, ssl
 from bs4 import BeautifulSoup
+
+sender_email = "scrapeprojectnotifier@gmail.com"
+receiver_email = "kenneth.lember@ametikool.ee"
+message = """\
+Subject: SkyScrape toote hinna teavitus
+a
+Teie valitud toote hind on langenud alla soovitud vaartuse, mine osta!
+
+SkyScrape."""
+port = 465  # For SSL
+password = input("Emaili konto salasõna: ")
+
+
+
 
 while True:
     try:
@@ -25,11 +40,17 @@ def priceChecker(sc):
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     currentPrice = soup.find("span", class_="b-product-price-current-number")['content']
-    if (currentPrice != initialPrice):
-        if(desiredPrice<=currentPrice):
+    print(currentPrice)
+    if (1):
+        if(int(currentPrice)<=desiredPrice):
             print("Osta!")
+            # Create a secure SSL context
+            context = ssl.create_default_context()
+            with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
+                server.login(sender_email, password)
+                server.sendmail(sender_email, receiver_email, message)
 
-    s.enter(86400, 1, priceChecker, (sc,))
+    s.enter(60, 1, priceChecker, (sc,))
 
-s.enter(86400, 1, priceChecker, (s,))
+s.enter(60, 1, priceChecker, (s,))
 s.run()
