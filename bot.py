8d40521c2cf0cdef7969ea31c2e368a7c405baf2
erki.tammeaid <erkipoiss01@gmail.com>
2021-09-30 13:20:58 +0300

    uuendus

--- bot.py
+++ bot.py
@@ -2,6 +2,14 @@ import requests
 import sched, time
 from bs4 import BeautifulSoup
 
+while True:
+    try:
+        desiredPrice = float(input("Sisestage soovitud hind: "))
+        print(desiredPrice)
+        break
+    except:
+        print("Peab olema number")
+
 URL = "https://laane.barbora.ee/toode/kohvioad-kronung-jacobs-1-kg"
 page = requests.get(URL)
 
@@ -13,15 +21,15 @@ print(initialPrice)
 
 s = sched.scheduler(time.time, time.sleep)
 def priceChecker(sc): 
-    print("Checking price..")
+    print("Kontrollin hinda..")
     page = requests.get(URL)
     soup = BeautifulSoup(page.content, "html.parser")
     currentPrice = soup.find("span", class_="b-product-price-current-number")['content']
     if (currentPrice != initialPrice):
-        if(currentPrice<initialPrice):
-            print("Hind on langenud")
+        if(desiredPrice<=currentPrice):
+            print("Osta!")
 
-    s.enter(300, 1, priceChecker, (sc,))
+    s.enter(86400, 1, priceChecker, (sc,))
 
-s.enter(300, 1, priceChecker, (s,))
+s.enter(86400, 1, priceChecker, (s,))
 s.run()
